3feea25776dcc1adc7f3a854ba89328067a78bdf
AKHILESHKHOSE <akhikhose3@gmail.com>
2024-02-24 00:05:03 +0530

    PROPER FILE SQL

--- PROJECT_1_CORRECTED.sql
+++ PROJECT_1_CORRECTED.sql
@@ -1,15 +1,19 @@
 --  PROJECT NAME: SPORT-ANALYTICS 
 --  LANGUAGE :   SQL(structured Query Language)
 /*  
-  PROJECT OVERVIEW:  
-  					
-  The Project aims to draw valuable INSIGHTS from  Data of Matches played in IPL
-  like BEST PERFORMING TEAM,PLAYERS AND  DATA MANIPULAION IF
-  FOUND INCORRECT. 
-  
-*/
+ PROJECT OVERVIEW:  
+ 
+ The Project aims to draw valuable INSIGHTS from  Data of Matches played in IPL
+ like BEST PERFORMING TEAM,PLAYERS AND  DATA MANIPULAION IF
+ FOUND INCORRECT. 
+ 
+ */
+
+ 
 --1
---CREATING 'matches' TABLE IN A NON-PUBLIC SCHEMA 'myschema'  WITH REQUIRED FIELDS MENTIONED IN PROJECT_STATEMENT.txt
+--CREATING 'matches' TABLE IN A NON-PUBLIC SCHEMA 'myschema'  
+--WITH REQUIRED FIELDS MENTIONED IN PROJECT_STATEMENT.txt
+
 CREATE TABLE myschema.matches (
     id bigint PRIMARY KEY,
     city varchar,
@@ -33,9 +37,9 @@ CREATE TABLE myschema.matches (
 SELECT * FROM myschema.matches;
 
 --2
+--CREATING 'deliveries' TABLE IN A NON-PUBLIC SCHEMA 'myschema'  WITH REQUIRED FIELDS MENTIONED 
+--IN PROJECT_STATEMENT.txt
 
---CREATING 'deliveries' TABLE IN A NON-PUBLIC SCHEMA 'myschema'  WITH REQUIRED FIELDS MENTIONED IN PROJECT_STATEMENT.txt
--- VERIFICATION
 CREATE TABLE myschema.deliveries (
     id bigint,
     FOREIGN KEY (id) REFERENCES myschema.matches,
@@ -61,7 +65,7 @@ SELECT * FROM myschema.deliveries;
 
 ---STEP 3
 
---IMPORT DATA FROM CSV FILES AND VERIFYING
+--IMPORT DATA FROM CSV FILES AND VERIFYING THEM
 --3
 COPY myschema.matches FROM 'C:\Program Files\PostgreSQL\16\data\PROJECT_INTERNHSALA\IPL_matches.csv' DELIMITER ',' CSV HEADER;
 
@@ -73,7 +77,6 @@ SELECT * FROM myschema.deliveries;
 --4
 COPY myschema.deliveries FROM 'C:\Program Files\PostgreSQL\16\data\PROJECT_INTERNHSALA\IPL_Ball.csv' DELIMITER ',' CSV HEADER;
 
-
 --ALTER COMMAND IS USED TO RE-DEFINE DATATYPE OF dismissal_kind to char(50) 
 --AS FOR SOME DATA THE REQIURED  STRING LENGTH  IS MORE THAN DECLARED 
 -- COPY AGAIN AFTER THE CHANGE
@@ -82,23 +85,15 @@ ALTER TABLE myschema.deliveries ALTER COLUMN dismissal_kind TYPE char(50);
 
 
 --5
---FIRST 20 MATCHES THAT WERE PLAYED IN IPL 
---ALIAS IS JUST USED  AS SUCH NOT NEEDED
-
-
+--FIRST 20 MATCHES THAT WERE PLAYED IN IPL
 SELECT * FROM myschema.matches AS a ORDER BY a.id, a.date LIMIT 20;
-
-SELECT * FROM myschema.deliveries WHERE id=335982 order by inning,over,ball;
 
 
 
 --6
 --FIRST 20 BALLS OF THE 1st GAME
---ALIAS IS JUST USED  AS SUCH NOT
 
 SELECT * FROM myschema.deliveries AS a ORDER BY a.id, a.inning, a.over, a.ball LIMIT 20;
-
-SELECT * FROM myschema.matches order by id,date;
 
 
 
@@ -110,7 +105,7 @@ SELECT * FROM myschema.matches WHERE date = '2013-05-02';
 
 --8
 --TEAMS WINNING THE MATCHES ON BASIS OF 'RUNS > 100'
-SELECT DISTINCT * FROM myschema.matches WHERE result = 'runs' AND result_margin > 100;
+SELECT * FROM myschema.matches WHERE result = 'runs' AND result_margin > 100;
 
 
 
@@ -118,15 +113,11 @@ SELECT DISTINCT * FROM myschema.matches WHERE result = 'runs' AND result_margin 
 --TIED MATCHES ORDER BY DATE DESC ORDER
 SELECT * FROM myschema.matches WHERE winner = 'NA' ORDER BY date DESC;
 
-SELECT * FROM myschema.matches WHERE result='tie' and date='2020-10-18;
-
-
 
 --10
 --COUNT OF CITIES HOSTING IPL MATCHES
 
 SELECT COUNT(DISTINCT city) AS NO_OF_HOST_CITY FROM myschema.matches;
-
 
 
 --NEW TABLE WITH NEW COLUMN AS ball_result
@@ -136,6 +127,7 @@ SELECT * FROM myschema.deliveries;
 
 
 --11
+--ADD NEW COLUMN 'ball result' TO deliveries_v02;
 CREATE TABLE myschema.deliveries_v02 AS 	
 SELECT
     deliveries.*,
@@ -157,12 +149,13 @@ SELECT * FROM myschema.deliveries_v02;
 --12
 --FIND TOTAL NO. OF BOUNDARIES and dot balls in all of the matches played.
 
-SELECT  ball_result, COUNT(ball_result) FROM myschema.deliveries_v02 GROUP BY ball_result;
+SELECT ball_result, COUNT(ball_result) FROM myschema.deliveries_v02 GROUP BY ball_result;
 
 
 
 --13
 --NO. OF BOUNDARIES SCORED BY EACH TEAM (DESCENDING ORDER)
+
 
 SELECT batting_team, ball_result, COUNT(ball_result) FROM myschema.deliveries_v02 WHERE ball_result = 'boundary' GROUP BY batting_team, ball_result ORDER BY COUNT(ball_result) DESC;
 
@@ -177,7 +170,7 @@ SELECT bowling_team, ball_result, COUNT(ball_result) FROM myschema.deliveries_v0
 
 --15
 --TOTAL DISMISSALS WHERE KIND IS NOT NA
-SELECT COUNT(dismissal_kind) FROM myschema.deliveries WHERE dismissal_kind !='NA' ;
+SELECT COUNT(dismissal_kind) FROM myschema.deliveries WHERE dismissal_kind != 'NA' ;
 
 
 
@@ -189,12 +182,10 @@ SELECT * FROM myschema.deliveries;
 SELECT bowler, SUM(is_wicket) AS TOTAL_WICKETS FROM myschema.deliveries WHERE dismissal_kind != 'run out' GROUP BY bowler ORDER BY SUM(is_wicket) DESC LIMIT 5;
 
 
-SELECT bowler, SUM(extra_runs) AS TOTAL_WICKETS FROM myschema.deliveries WHERE dismissal_kind != 'run out' GROUP BY bowler ORDER BY SUM(extra_runs) DESC LIMIT 5;
-
 
 --17
---CREATE deliveries_v03 WITH ALL DATA OF  'deliveries_v02' AND
--ALSO ADD 'venue' and 'date' 
+--CREATE deliveries_v03 WITH ALL DATA OF  ' deliveries_v02 ' AND
+-ALSO ADD ' venue ' and ' date ' 
 
 CREATE TABLE myschema.deliveries_v03 AS
 SELECT a.*, b.venue, b.date FROM myschema.deliveries_v02 AS a INNER JOIN myschema.matches AS b ON a.id = b.id;
@@ -213,29 +204,36 @@ SELECT venue, SUM(total_runs) AS TOTAL_RUNS FROM myschema.deliveries_v03 GROUP B
 SELECT venue, date, SUM(total_runs) AS TOTAL_RUNS FROM myschema.deliveries_v03 WHERE venue = 'Eden Gardens' GROUP BY venue, date ORDER BY SUM(total_runs) DESC;
 
 
+
 --20
 --CORRECT TEAM NAME RISING PUNE SUPERGIANTS 
 SELECT * FROM myschema.matches;
 
 
 CREATE TABLE matches_corrected AS
-SELECT *, REPLACE(team1, 'Supergiant', 'Supergiants') AS team1_corr, REPLACE(team2, 'Supergiant', 'Supergiants') AS team2_corr FROM myschema.matches;
+SELECT *, REPLACE(team1, ' Supergiant ', ' Supergiants ') AS team1_corr, REPLACE(team2, ' Supergiant ', ' Supergiants ') AS team2_corr FROM myschema.matches;
+
 
 
 --21
---COMBINE MATCH_ID, INNING, OVER, BALL SEPARATED BY  '-'
+--COMBINE MATCH_ID, INNING, OVER, BALL SEPARATED BY  ' - '
 
 SELECT * FROM myschema.deliveries_v03;
 
 CREATE TABLE myschema.deliveries_v04 AS
-SELECT id || '-' || inning || '-' || over || '-' || ball AS ball_id, batsman, non_striker, bowler, batsman_runs, extra_runs, total_runs,
-is_wicket, dismissal_kind, player_dismissed, fielder, extras_type, batting_team, bowling_team, ball_result, venue, date  FROM myschema.deliveries_v03 ;
+SELECT id || ' - ' || inning || ' - ' || over || ' - ' || ball AS ball_id,
+ batsman, non_striker, bowler, batsman_runs, extra_runs, total_runs,
+is_wicket, dismissal_kind, player_dismissed, fielder, extras_type,
+ batting_team, bowling_team, ball_result, venue, date  FROM myschema.deliveries_v03 ;
 
 --OR
 
 CREATE TABLE myschema.deliveries_v04 AS
-SELECT CONCAT(id,'-',inning,'-',over,'-',ball) AS ball_id, batsman, non_striker, bowler, batsman_runs, extra_runs, total_runs,
-is_wicket, dismissal_kind, player_dismissed, fielder, extras_type, batting_team, bowling_team, ball_result, venue, date  FROM myschema.deliveries_v03 ;
+SELECT CONCAT(id,' - ',inning,' - ',over,' - ',ball) AS ball_id,
+ batsman, non_striker, bowler, batsman_runs, extra_runs, total_runs,
+is_wicket, dismissal_kind, player_dismissed, fielder, extras_type,
+ batting_team, bowling_team, ball_result, venue, date  FROM myschema.deliveries_v03 ;
+
 
 
 --22
@@ -246,6 +244,7 @@ SELECT COUNT(DISTINCT ball_id) FROM myschema.deliveries_v04;
 SELECT COUNT(ball_id) FROM myschema.deliveries_v04;
 
 
+
 --23
 --USE ROW_NUMBER() FUNCTION TO ASSIGN UNQIUE NO. TO EACH 
 --ROW
@@ -253,9 +252,12 @@ CREATE TABLE myschema.deliveries_v05 AS
 SELECT ROW_NUMBER() OVER(PARTITION BY ball_id) AS r_num, a.* FROM myschema.deliveries_v04 AS a;
 
 
+
 --24
 --INSTANCES WHERE BALL_ID IS REPEATING
 SELECT * FROM myschema.deliveries_v05 WHERE r_num = 2;
+
+
 
 --25
 --DATA OF REPEATING BALL_ID
